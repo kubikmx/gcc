57dd72c3f3876f134a42670a7cfb9ea6a3d84cb7
Alfredo Pulido <alfredo@kubik.mx>
2015-05-28 17:46:30 -0500

    Guadalajara Country Club
    First charge

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function onLoad(){
+	//document.title = App.Properties.__caption;	
+	//document.getElementById('blockName').innerText = App.Properties.__caption;
+	loadinfo();
+}
+
+function loadinfo(){
+	$.getJSON("http://www.k-i.co/cc/webservices/noticias.php", function(result){
+        $.each(result, function(i, field){
+         var bloque=  '<div class="bordelist"><div class="articulo"><img src="http://k-i.co/cc/images/noticias/thumbs/'+field.imagen+'" >';
+         	 bloque+= '<p><span>'+ field.nombre +' </span>'+field.intro+'</p>';
+			 bloque+= '<a href="detalle.html" data-id="'+field.id_noticia+'"></a>';
+			 bloque+= '</div><div class="clear"></div></div>';	
+
+		$("#listado_normal").append(bloque);	 
+
+        });
+    });
+
+
+}
