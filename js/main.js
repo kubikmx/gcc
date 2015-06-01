284e19e3d90cd997256856667f829fd18dc52275
Alfredo Pulido <alfredo@kubik.mx>
2015-06-01 12:34:14 -0500

    paso de variables

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function loadinfo(){
         $.each(result, function(i, field){
          var bloque=  '<div class="bordelist"><div class="articulo"><img src="http://k-i.co/cc/images/noticias/thumbs/'+field.imagen+'" >';
          	 bloque+= '<p><span>'+ field.nombre +' </span>'+field.intro+'</p>';
-			 bloque+= '<a href="detalle.html" data-id="'+field.id_noticia+'"></a>';
+			 bloque+= '<a href="detalle.html?id='+field.id_noticia+'" data-id="'+field.id_noticia+'"></a>';
 			 bloque+= '</div><div class="clear"></div></div>';	
 
 		$("#listado_normal").append(bloque);	 
