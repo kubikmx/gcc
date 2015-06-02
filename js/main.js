99ddec6e50f6cc9d66dd424c686d7394622b374d
kubiklex <alex@kubik.mx>
2015-06-02 11:52:20 -0500

    avisos

--- js/main.js
+++ js/main.js
@@ -14,6 +14,18 @@ function loadinfo(){
 
         });
     });
+}
 
+function loadavisos(){
+	$.getJSON("http://www.k-i.co/cc/webservices/eventos.php", function(result){
+        $.each(result, function(i, field){
+         var bloque=  '<div class="bordelist"><div class="articulo"><img src="http://k-i.co/cc/images/eventos/thumbs/'+field.imagen+'" >';
+         	 bloque+= '<p><span>'+ field.nombre +' </span>'+field.intro+'</p>';
+			 bloque+= '<a href="detalle_aviso.html?idv='+field.id_evento+'" data-id="'+field.id_evento+'"></a>';
+			 bloque+= '</div><div class="clear"></div></div>';	
 
+		$("#listado_eventos").append(bloque);
+
+        });
+    });
 }
