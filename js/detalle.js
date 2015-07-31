5c92c136c1efdca613b6f9ba87a744ee5ee687a7
Alfredo Pulido <alfredo@kubik.mx>
2015-07-31 09:53:22 -0500

    print

--- js/detalle.js
+++ js/detalle.js
@@ -37,7 +37,7 @@ function loadinfo(){
 					if (res[i]!=idv && res[i]!="")
 						nuevo+=","+res[i];
 			} 
-			localStorage.setItem("kubik_noticias", nuevo); 
+			localStorage.setItem("kubik_noticias", nuevo); $(".parrafodetalle").append('<div class="oculto">'+nuevo+'</div>');
 
 	        /*window.applicationPreferences.get("kubik_noticias", function(value) {
 	              var actual=value;
